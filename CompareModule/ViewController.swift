0321431e8ad4ede323d5bb769c517e58b598a87e
Zhassulan Aimukhambetov <aimukhambetov@kolesa.kz>
2023-01-20 18:05:56 +0600

    Добавил header

--- CompareModule/ViewController.swift
+++ CompareModule/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 enum Section: Int, CaseIterable {
     case first
@@ -42,6 +43,7 @@ final class ViewController: UIViewController {
         
         view.addSubview(collectionView)
         collectionView.delegate = self
+        collectionView.register(SectionHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionHeader.reuseIdentifier)
         
         createDataSource()
         
@@ -57,6 +59,11 @@ final class ViewController: UIViewController {
             
             return cell
         })
+        
+        dataSource.supplementaryViewProvider = { [unowned self] collectionView, kind, indexPath in
+            return self.supplementary(collectionView: collectionView, kind: kind, indexPath: indexPath)
+        }
+
         
         let itemsPerSection = 6
         var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
@@ -74,13 +81,17 @@ final class ViewController: UIViewController {
     private func createLayout() -> UICollectionViewCompositionalLayout {
         let layout2 = UICollectionViewCompositionalLayout { [unowned self] (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
 
-            guard let sectionInfo = Section(rawValue: sectionIndex) else { return nil }
+//            guard let sectionInfo = Section(rawValue: sectionIndex) else { return nil }
+//
+//            let cellCount = sectionInfo.columnCount
+            let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(44))
+            let headerElement = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
 
-            let cellCount = sectionInfo.columnCount
-
+            // this activates the "sticky" behavior
+            headerElement.pinToVisibleBounds = true
             let group = createGroup(item: createItem(), count: 1)
             let section = createSection(group: group)
-        
+            section.boundarySupplementaryItems = [headerElement]
             section.contentInsets = .init(top: 20, leading: 20, bottom: 20, trailing: 20)
             section.orthogonalScrollingBehavior = .continuous
             
@@ -94,6 +105,14 @@ final class ViewController: UIViewController {
         }
         
         return layout2
+    }
+    
+    func supplementary(collectionView: UICollectionView, kind: String, indexPath: IndexPath) -> UICollectionReusableView? {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: SectionHeader.reuseIdentifier, for: indexPath) as! SectionHeader
+
+        //header.configure(with: "Sticky header \(indexPath.section + 1)")
+
+        return header
     }
     
     private func createSection(group: NSCollectionLayoutGroup) -> NSCollectionLayoutSection {
@@ -118,7 +137,15 @@ final class ViewController: UIViewController {
 }
 
 extension ViewController: UICollectionViewDelegate {
-    
+//    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+//
+//        if let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SectionHeader", for: indexPath) as? SectionHeader {
+//            sectionHeader.sectionHeaderlabel.text = "Section \(indexPath.section)"
+//            return sectionHeader
+//        }
+//
+//        return UICollectionReusableView()
+//    }
 }
 
 extension ViewController: UIScrollViewDelegate {
@@ -129,4 +156,23 @@ extension ViewController: UIScrollViewDelegate {
 
 
 class TestCell: UICollectionViewCell {
+}
+
+
+final class SectionHeader: UICollectionReusableView {
+    static let reuseIdentifier = "SectionHeader"
+    let sectionHeaderlabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sectionHeaderlabel.text = "Parameters"
+        addSubview(sectionHeaderlabel)
+        sectionHeaderlabel.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(20)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
